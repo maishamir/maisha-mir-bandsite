31a72e299f74cee0ae58397d7e61a806fe49e2ab
Maisha <maisha.mir2014@gmail.com>
2024-05-29 22:23:32 -0700

    post request working as of now

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,6 +1,7 @@
 const baseURL = "https://project-1-api.herokuapp.com/"
 const API_KEY = "faead40e-7b40-4ae3-b771-67cac927598d"
 const commentDiv = document.querySelector(".comments")
+const addCommentForm = document.querySelector(".form")
 
 
 class BandSiteAPI {
@@ -12,7 +13,6 @@ class BandSiteAPI {
         try {
             const reqResp = await axios.get(`${this.baseURL}comments?api_key=${this.apiKey}`)
             const commentObjArr = reqResp.data;
-            console.log(commentObjArr)
 
             commentObjArr.forEach((comment) => {
                 const commentContainer = createTagWithClass("div", "comments-container")
@@ -51,8 +51,43 @@ class BandSiteAPI {
         
     }
 
-    
+    async postComment(commentObj) {
+        try {
+            console.log(commentObj)
+            await axios.post(`${this.baseURL}comments?api_key=${this.apiKey}`, commentObj)
+            // const newComment = reqResp.data;
+            this.getComments();
+
+            // I could also do this
+            // const requestResp = await axios.post(`${this.baseURL}comments?api_key=${this.apiKey}`, commentObj)
+            //const newComment requestResp.data;
+            // this.displayComment(newComment)
+        } catch (error) {
+            if (error.reqResp) {
+                if (error.reqResp.status === 400) {
+                    console.error("Either the name or the comment was included. Please double check before adding comment.", error)
+                } else {
+                    console.error("Error occurred while posting the comment.", error)
+                }
+            } else {
+                console.error("There was an error while posting the comment")
+            }
+        }
+    }
 }
+
+addCommentForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const commentObject = {};
+    const name = event.target.name.value;
+    const comment = event.target.comment.value;
+    commentObject.name = name;
+    commentObject.comment = comment;
+
+    bandsiteAPI.postComment(commentObject)
+
+})
+
 
 function createTagWithClass(tag, className) {
     const tagEl = document.createElement(tag);
